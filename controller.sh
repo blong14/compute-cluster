6bad0a99b416a5c4d2e819f0396a31c4b58bd778
Ben Long <14benj@gmail.com>
2021-04-03 11:24:12 -0400

    Update controller.sh

--- controller.sh
+++ controller.sh
@@ -16,6 +16,7 @@ cp charts/repositories.yaml ~/.config/helm
 cp charts/repositories.lock ~/.config/helm
 
 # configure helm and kubectl
+mkdir -p etc/rancher/k3s
 sudo cp /etc/rancher/k3s/k3s.yaml etc/rancher/k3s/k3s.yaml
 sudo chown pi:pi etc/rancher/k3s/k3s.yaml
 cp etc/.bash_profile ~/.bash_profile
