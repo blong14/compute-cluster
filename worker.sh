fca6fc162871725ec7043a6af9360dedeb6126f0
Ben Long <14benj@gmail.com>
2021-04-03 11:22:53 -0400

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -2,14 +2,13 @@
 
 sudo apt update && apt install -y openssh-server net-tools  
 
-
 K3S_URL=$(cat etc/controller-ip.conf | awk '{print $2}')
 K3S_TOKEN=$(cat etc/network.conf)
-
 
 curl -sfL https://get.k3s.io | K3S_URL="https://$K3S_URL:6443" K3S_TOKEN=$K3S_TOKEN sh -
 
 # configure kubectl
 sudo cp /etc/rancher/k3s/k3s.yaml etc/rancher/k3s/k3s.yaml
 sudo chown pi:pi etc/rancher/k3s/k3s.yaml
-export KUBECONFIG=~/Developer/compute-cluster/etc/rancher/k3s/k3s.yaml
+cp etc/.bash_profile ~/.bash_profile
+source ~/.bash_profile
