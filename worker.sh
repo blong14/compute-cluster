ce721695c55987b6533f10d5241a2b45410adfa7
Ben Long <14benj@gmail.com>
2021-04-03 11:24:31 -0400

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -8,6 +8,7 @@ K3S_TOKEN=$(cat etc/network.conf)
 curl -sfL https://get.k3s.io | K3S_URL="https://$K3S_URL:6443" K3S_TOKEN=$K3S_TOKEN sh -
 
 # configure kubectl
+mkdir -p etc/rancher/k3s
 sudo cp /etc/rancher/k3s/k3s.yaml etc/rancher/k3s/k3s.yaml
 sudo chown pi:pi etc/rancher/k3s/k3s.yaml
 cp etc/.bash_profile ~/.bash_profile
