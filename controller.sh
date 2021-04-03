24fa638f732f07015af86fdbcb299e3b8a443dc8
Ben Long <14benj@gmail.com>
2021-04-03 11:07:43 -0400

    Update controller.sh

--- controller.sh
+++ controller.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# ubuntu specific
-# sudo apt update && apt install -y openssh-server
+sudo apt update && apt install -y openssh-server net-tools  
 
 # install k3s
 curl -sfL https://get.k3s.io | sh -
 
 
 # install helm
-mkdir -R ~/.config/helm
+mkdir ~/.config/helm
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 
 cp charts/repositories.yaml ~/.config/helm
 cp charts/repositories.lock ~/.config/helm
+
+# configure helm and kubectl
+sudo cp /etc/rancher/k3s/k3s.yaml etc/rancher/k3s/k3s.yaml
+sudo chown pi:pi etc/rancher/k3s/k3s.yaml
+export KUBECONFIG=~/Developer/compute-cluster/etc/rancher/k3s/k3s.yaml
