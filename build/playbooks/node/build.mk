1934e99d3bc8d84e492f3eca532a371ea5d50f22
Ben Long <blong14@lemur.localdomain>
2023-04-24 19:17:00 -0400

    Repo cleanup

--- build/playbooks/node/build.mk
+++ build/playbooks/node/build.mk
@@ -8,3 +8,6 @@ update-node:
 
 upgrade-node:
 	ansible-playbook build/playbooks/node/upgrade-release.yml -f 1 -u pi --become -K -vv
+
+upgrade-k3s:
+	ansible-playbook build/playbooks/node/upgrade-k3s.yml -u pi --become -K -vv
--- worker.sh
+++
@@ -1,1 +0,0 @@
-curl -sfL https://get.k3s.io | K3S_TOKEN=$(cat /etc/node-token) K3S_URL=https://compute.cluster:6443 sh -s
