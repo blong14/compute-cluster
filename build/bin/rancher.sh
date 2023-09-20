6f93b5b2f382e8310c9baff62728295f5d31e11c
Ben Long <blong14@lemur.localdomain>
2023-09-19 22:30:03 -0400

    Converting rancher helm install to ansible playbook

--- build/bin/rancher.sh
+++
@@ -1,7 +0,0 @@
-helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
-
-helm install rancher rancher-stable/rancher \
-  --kubeconfig /etc/rancher/k3s/k3s.yaml \
-  --namespace cattle-system \
-  --set hostname=compute.cluster \
-  --set replicas=1
