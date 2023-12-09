f6c34e1a3d2a75de7d9dd8bb9ab99ff0eca1e8ee
Ben Long <blong14@lemur.localdomain>
2023-12-08 23:43:14 -0500

    Updating log consumer

--- src/internal/c/logconsumer/main.c
+++ src/internal/c/logconsumer/main.c
@@ -7,11 +7,6 @@
 #include "amqp.h"
 #include "amqp_tcp_socket.h"
 
-#define PORT 5672
-#define QUEUE "log"
-// #define EXCHANGE "amq.direct"
-// #define BINDINGKEY "news-summary"
-
 bool check_reply(amqp_rpc_reply_t);
 void consume(amqp_connection_state_t);
 
@@ -19,6 +14,25 @@ int main(void)
 {
 	amqp_socket_t *socket = NULL;
 	amqp_bytes_t queuename;
+	const char *host, *port, *user, *password, *queue;
+
+	if ((host = getenv("RMQ_HOST")) == NULL)
+		host = "localhost";
+
+	if ((port = getenv("RMQ_PORT")) == NULL)
+		port = "5672";
+
+	if ((user = getenv("RMQ_USER")) == NULL) {
+		user = "guest";
+	}
+
+	if ((password = getenv("RMQ_PASSWORD")) == NULL) {
+		password = "guest";
+	}
+
+	if ((queue = getenv("RMQ_QUEUE")) == NULL) {
+		queue = "log";
+	}
 
 	const amqp_connection_state_t conn = amqp_new_connection();
 	socket = amqp_tcp_socket_new(conn);
@@ -27,20 +41,12 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-	const char *host = getenv("RMQ_HOST");
-	if (host == NULL) host = "localhost";
-
-	int status = amqp_socket_open(socket, host, PORT);
+	printf("opening socket @ amqp://%s:%s\n", host, port);
+	int status = amqp_socket_open(socket, host, atoi(port));
 	if (AMQP_STATUS_OK != status) {
 		perror("error opening TCP socket");
 		exit(status);
 	}
-
-	const char *user = getenv("RMQ_USER");
-	if (user == NULL) user = "guest";
-
-	const char *password = getenv("RMQ_PASSWORD");
-	if (password == NULL) password = "guest";
 
 	amqp_rpc_reply_t r = amqp_login(
 		conn, "/", 0, AMQP_DEFAULT_FRAME_SIZE, 0, AMQP_SASL_METHOD_PLAIN, user, password);
@@ -56,7 +62,7 @@ int main(void)
 	}
 
 	const amqp_queue_declare_ok_t *queue_declare_ok =  amqp_queue_declare(
-		conn, 1, amqp_cstring_bytes(QUEUE), 0, 0, 0, 1, amqp_empty_table);
+		conn, 1, amqp_cstring_bytes(queue), 0, 0, 0, 1, amqp_empty_table);
 	if (!check_reply(amqp_get_rpc_reply(conn))) {
 		perror("error declaring queue");
 		exit(EXIT_FAILURE);
@@ -68,14 +74,6 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-	// amqp_queue_bind(conn, 1, queuename, amqp_cstring_bytes(EXCHANGE),
-	// 				amqp_cstring_bytes(BINDINGKEY), amqp_empty_table);
-	// if (!check_reply(amqp_get_rpc_reply(conn))) {
-	// 	fprintf(stderr, "error binding to queue");
-	// 	perror("error binding to queue");
-	// 	exit(EXIT_FAILURE);
-	// }
-
 	amqp_basic_consume(conn, 1, queuename, amqp_empty_bytes, 0, 1, 0,
 					   amqp_empty_table);
 	if (!check_reply(amqp_get_rpc_reply(conn))) {
@@ -83,7 +81,7 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-	puts("rmq client connected...");
+	fprintf(stdout, "consuming on %s\n", (const char *)queuename.bytes);
 	consume(conn);
 
 	amqp_bytes_free(queuename);
@@ -112,8 +110,6 @@ bool check_reply(amqp_rpc_reply_t reply)
 
 void consume(amqp_connection_state_t conn)
 {
-	puts("consuming...");
-
 	amqp_frame_t frame;
 
 	for (;;) {
@@ -132,24 +128,6 @@ void consume(amqp_connection_state_t conn)
 
 				if (AMQP_FRAME_METHOD == frame.frame_type) {
 					switch (frame.payload.method.id) {
-						case AMQP_BASIC_ACK_METHOD:
-							/* if we've turned publisher confirms on, and we've published a
-							 * message here is a message being confirmed.
-							 */
-							break;
-						case AMQP_BASIC_RETURN_METHOD:
-							/* if a published message couldn't be routed and the mandatory
-							 * flag was set this is what would be returned. The message then
-							 * needs to be read.
-							 */
-							{
-								amqp_message_t message;
-								if (!check_reply(amqp_read_message(conn, frame.channel, &message, 0))) {
-									return;
-								}
-								amqp_destroy_message(&message);
-							}
-							break;
 						case AMQP_CHANNEL_CLOSE_METHOD:
 							/* a channel.close method happens when a channel exception occurs,
 							 * this can happen by publishing to an exchange that doesn't exist
@@ -175,12 +153,12 @@ void consume(amqp_connection_state_t conn)
 				}
 			}
 		} else {
-			if(!amqp_basic_ack(conn, 1, 0, 0)) {
+			amqp_bytes_t body = envelope.message.body;
+			if(!amqp_basic_ack(conn, envelope.channel, envelope.delivery_tag, 0)) {
+				fprintf(stderr, "basic ack failed %s\n", (const char *)body.bytes);
 				puts("basic ack failed");
 			}
-			amqp_bytes_t body = envelope.message.body;
-			printf("received message with length %lu", body.len);
-			printf("message %s", (const char *)body.bytes);
+			fprintf(stdout, "message received %s", (const char *)body.bytes);
 			amqp_destroy_envelope(&envelope);
 		}
 	}
