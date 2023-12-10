2b0c955b0ec958144affdda919febb8a3e806642
Ben Long <blong14@lemur.localdomain>
2023-12-09 23:26:20 -0500

    Code cleanup

--- src/internal/c/logconsumer/main.c
+++ src/internal/c/logconsumer/main.c
@@ -2,163 +2,213 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 #include "amqp.h"
 #include "amqp_tcp_socket.h"
 
-bool check_reply(amqp_rpc_reply_t);
-void consume(amqp_connection_state_t);
+typedef struct {
+    const char* host;
+    const char* port;
+    const char* user;
+    const char* password;
+    const char* queue;
+} rmq_env;
 
-int main(void)
-{
-	amqp_socket_t *socket = NULL;
-	amqp_bytes_t queuename;
-	const char *host, *port, *user, *password, *queue;
+amqp_connection_state_t rmq_connect(rmq_env);
 
-	if ((host = getenv("RMQ_HOST")) == NULL)
-		host = "localhost";
+amqp_bytes_t rmq_declare(amqp_connection_state_t, const char* queue);
 
-	if ((port = getenv("RMQ_PORT")) == NULL)
-		port = "5672";
+void rmq_consume(amqp_connection_state_t, amqp_bytes_t);
 
-	if ((user = getenv("RMQ_USER")) == NULL) {
-		user = "guest";
-	}
+void rmq_close(amqp_connection_state_t, amqp_bytes_t);
 
-	if ((password = getenv("RMQ_PASSWORD")) == NULL) {
-		password = "guest";
-	}
+bool rmq_check_reply(const amqp_rpc_reply_t*);
 
-	if ((queue = getenv("RMQ_QUEUE")) == NULL) {
-		queue = "log";
-	}
+rmq_env new_rmq_env(void);
 
-	const amqp_connection_state_t conn = amqp_new_connection();
-	socket = amqp_tcp_socket_new(conn);
-	if (!socket) {
-		perror("error creating TCP socket");
-		exit(EXIT_FAILURE);
-	}
+int main(void) {
+    const rmq_env env = new_rmq_env();
 
-	printf("opening socket @ amqp://%s:%s\n", host, port);
-	int status = amqp_socket_open(socket, host, atoi(port));
-	if (AMQP_STATUS_OK != status) {
-		perror("error opening TCP socket");
-		exit(status);
-	}
+    const amqp_connection_state_t conn = rmq_connect(env);
 
-	amqp_rpc_reply_t r = amqp_login(
-		conn, "/", 0, AMQP_DEFAULT_FRAME_SIZE, 0, AMQP_SASL_METHOD_PLAIN, user, password);
-	if (!check_reply(r)) {
-		perror("error logging in");
-		exit(EXIT_FAILURE);
-	}
+    const amqp_bytes_t queuename = rmq_declare(conn, env.queue);
 
-	amqp_channel_open(conn, 1);
-	if (!check_reply(amqp_get_rpc_reply(conn))) {
-		perror("error opening channel");
-		exit(EXIT_FAILURE);
-	}
+    rmq_consume(conn, queuename);
 
-	const amqp_queue_declare_ok_t *queue_declare_ok =  amqp_queue_declare(
-		conn, 1, amqp_cstring_bytes(queue), 0, 0, 0, 1, amqp_empty_table);
-	if (!check_reply(amqp_get_rpc_reply(conn))) {
-		perror("error declaring queue");
-		exit(EXIT_FAILURE);
-	}
+    rmq_close(conn, queuename);
 
-	queuename = amqp_bytes_malloc_dup(queue_declare_ok->queue);
-	if (queuename.bytes == NULL) {
-		perror("out of memory while copying queue name");
-		exit(EXIT_FAILURE);
-	}
-
-	amqp_basic_consume(conn, 1, queuename, amqp_empty_bytes, 0, 1, 0,
-					   amqp_empty_table);
-	if (!check_reply(amqp_get_rpc_reply(conn))) {
-		perror("error in basic consume");
-		exit(EXIT_FAILURE);
-	}
-
-	fprintf(stdout, "consuming on %s\n", (const char *)queuename.bytes);
-	consume(conn);
-
-	amqp_bytes_free(queuename);
-	amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS);
-	amqp_connection_close(conn, AMQP_REPLY_SUCCESS);
-	amqp_destroy_connection(conn);
-
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
-bool check_reply(amqp_rpc_reply_t reply)
-{
-	switch (reply.reply_type) {
-		case AMQP_RESPONSE_NORMAL:
-			return true;
-		case AMQP_RESPONSE_SERVER_EXCEPTION:
-			fprintf(stderr, "AMQP server exception\n");
-			return false;
-		case AMQP_RESPONSE_LIBRARY_EXCEPTION:
-			fprintf(stderr, "AMQP library exception\n");
-			return false;
-		default:
-			return false;
-	}
+amqp_connection_state_t rmq_connect(const rmq_env env) {
+    amqp_connection_state_t conn;
+    amqp_socket_t* socket;
+
+    printf("opening socket @ amqp://%s:%s\n", env.host, env.port);
+
+    if ((conn = amqp_new_connection()) == NULL) {
+        perror("error creating connection");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((socket = amqp_tcp_socket_new(conn)) == NULL) {
+        perror("error creating TCP socket");
+        exit(EXIT_FAILURE);
+    }
+
+    const int status = amqp_socket_open(socket, env.host, atoi(env.port));
+    if (status != AMQP_STATUS_OK) {
+        perror("error opening TCP socket");
+        printf("%d/n", status);
+        exit(status);
+    }
+
+    const amqp_rpc_reply_t login_reply = amqp_login(
+        conn, "/", 0, AMQP_DEFAULT_FRAME_SIZE, 0, AMQP_SASL_METHOD_PLAIN, env.user, env.password);
+    if (!rmq_check_reply(&login_reply)) {
+        perror("error logging in");
+        exit(EXIT_FAILURE);
+    }
+
+    amqp_channel_open(conn, 1);
+    const amqp_rpc_reply_t chn_open_reply = amqp_get_rpc_reply(conn);
+    if (!rmq_check_reply(&chn_open_reply)) {
+        perror("error opening channel");
+        exit(EXIT_FAILURE);
+    }
+
+    return conn;
 }
 
-void consume(amqp_connection_state_t conn)
-{
-	amqp_frame_t frame;
+amqp_bytes_t rmq_declare(const amqp_connection_state_t conn, const char* queue) {
+    const amqp_queue_declare_ok_t* queue_declare_ok = amqp_queue_declare(
+        conn, 1, amqp_cstring_bytes(queue), 0, 0, 0, 1, amqp_empty_table);
+    const amqp_rpc_reply_t reply = amqp_get_rpc_reply(conn);
+    if (!rmq_check_reply(&reply)) {
+        perror("error declaring queue");
+        exit(EXIT_FAILURE);
+    }
 
-	for (;;) {
-		amqp_rpc_reply_t ret;
-		amqp_envelope_t envelope;
+    const amqp_bytes_t queuename = amqp_bytes_malloc_dup(queue_declare_ok->queue);
+    if (queuename.bytes == NULL) {
+        perror("out of memory while copying queue name");
+        exit(EXIT_FAILURE);
+    }
 
-		amqp_maybe_release_buffers(conn);
+    return queuename;
+}
 
-		ret = amqp_consume_message(conn, &envelope, NULL, 0);
-		if (!check_reply(ret)) {
-			if (AMQP_RESPONSE_LIBRARY_EXCEPTION == ret.reply_type &&
-				AMQP_STATUS_UNEXPECTED_STATE == ret.library_error) {
-				if (AMQP_STATUS_OK != amqp_simple_wait_frame(conn, &frame)) {
-					return;
-				}
+void rmq_consume(amqp_connection_state_t conn, amqp_bytes_t queuename) {
+    fprintf(stdout, "consuming on %s\n", (const char *) queuename.bytes);
 
-				if (AMQP_FRAME_METHOD == frame.frame_type) {
-					switch (frame.payload.method.id) {
-						case AMQP_CHANNEL_CLOSE_METHOD:
-							/* a channel.close method happens when a channel exception occurs,
-							 * this can happen by publishing to an exchange that doesn't exist
-							 * for example.
-							 *
-							 * In this case you would need to open another channel redeclare
-							 * any queues that were declared auto-delete, and restart any
-							 * consumers that were attached to the previous channel.
-							 */
-							return;
-						case AMQP_CONNECTION_CLOSE_METHOD:
-							/* a connection.close method happens when a connection exception
-							 * occurs, this can happen by trying to use a channel that isn't
-							 * open for example.
-							 *
-							 * In this case the whole connection must be restarted.
-							 */
-							return;
-						default:
-							fprintf(stderr, "An unexpected method was received %u", frame.payload.method.id);
-							return;
-					}
-				}
-			}
-		} else {
-			amqp_bytes_t body = envelope.message.body;
-			if(!amqp_basic_ack(conn, envelope.channel, envelope.delivery_tag, 0)) {
-				fprintf(stderr, "basic ack failed %s\n", (const char *)body.bytes);
-			}
-			fprintf(stdout, "message received %s\n", (const char *)body.bytes);
-			amqp_destroy_envelope(&envelope);
-		}
-	}
+    amqp_basic_consume(conn, 1, queuename, amqp_empty_bytes, 0, 1, 0,
+                       amqp_empty_table);
+    const amqp_rpc_reply_t cns_reply = amqp_get_rpc_reply(conn);
+    if (!rmq_check_reply(&cns_reply)) {
+        perror("error in basic consume");
+        exit(EXIT_FAILURE);
+    }
+
+    amqp_frame_t frame;
+
+    for (;;) {
+        amqp_rpc_reply_t ret;
+        amqp_envelope_t envelope;
+
+        amqp_maybe_release_buffers(conn);
+
+        ret = amqp_consume_message(conn, &envelope, NULL, 0);
+        if (!rmq_check_reply(&ret)) {
+            if (AMQP_RESPONSE_LIBRARY_EXCEPTION == ret.reply_type &&
+                AMQP_STATUS_UNEXPECTED_STATE == ret.library_error) {
+                if (AMQP_STATUS_OK != amqp_simple_wait_frame(conn, &frame)) {
+                    return;
+                }
+
+                if (AMQP_FRAME_METHOD == frame.frame_type) {
+                    switch (frame.payload.method.id) {
+                        case AMQP_CHANNEL_CLOSE_METHOD:
+                            /* a channel.close method happens when a channel exception occurs,
+                             * this can happen by publishing to an exchange that doesn't exist
+                             * for example.
+                             *
+                             * In this case you would need to open another channel redeclare
+                             * any queues that were declared auto-delete, and restart any
+                             * consumers that were attached to the previous channel.
+                             */
+                            return;
+                        case AMQP_CONNECTION_CLOSE_METHOD:
+                            /* a connection.close method happens when a connection exception
+                             * occurs, this can happen by trying to use a channel that isn't
+                             * open for example.
+                             *
+                             * In this case the whole connection must be restarted.
+                             */
+                            return;
+                        default:
+                            fprintf(stderr, "An unexpected method was received %u", frame.payload.method.id);
+                            return;
+                    }
+                }
+            }
+        } else {
+            amqp_bytes_t body = envelope.message.body;
+            if (!amqp_basic_ack(conn, envelope.channel, envelope.delivery_tag, 0)) {
+                fprintf(stderr, "basic ack failed %s\n", (const char *) body.bytes);
+            }
+            fprintf(stdout, "message received %s\n", (const char *) body.bytes);
+            amqp_destroy_envelope(&envelope);
+        }
+    }
+}
+
+void rmq_close(amqp_connection_state_t conn, amqp_bytes_t queuename) {
+    amqp_bytes_free(queuename);
+    amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS);
+    amqp_connection_close(conn, AMQP_REPLY_SUCCESS);
+    amqp_destroy_connection(conn);
+}
+
+rmq_env new_rmq_env(void) {
+    rmq_env env;
+    const char *host, *port, *user, *password, *queue;
+
+    if ((host = getenv("RMQ_HOST")) == NULL)
+        host = "localhost";
+
+    if ((port = getenv("RMQ_PORT")) == NULL)
+        port = "5672";
+
+    if ((user = getenv("RMQ_USER")) == NULL)
+        user = "guest";
+
+    if ((password = getenv("RMQ_PASSWORD")) == NULL)
+        password = "guest";
+
+    if ((queue = getenv("RMQ_QUEUE")) == NULL)
+        queue = "log";
+
+    env.host = host;
+    env.port = port;
+    env.user = user;
+    env.password = password;
+    env.queue = queue;
+
+    return env;
+}
+
+bool rmq_check_reply(const amqp_rpc_reply_t* reply) {
+    switch (reply->reply_type) {
+        case AMQP_RESPONSE_NORMAL:
+            return true;
+        case AMQP_RESPONSE_SERVER_EXCEPTION:
+            fprintf(stderr, "AMQP server exception\n");
+            return false;
+        case AMQP_RESPONSE_LIBRARY_EXCEPTION:
+            fprintf(stderr, "AMQP library exception\n");
+            return false;
+        default:
+            fprintf(stderr, "AMQP unknown exception\n");
+            return false;
+    }
 }
