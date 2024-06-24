83b4191ea57ddc876095f075bd2a0c74057a8309
Ben Long <blong14@lemur.localdomain>
2024-06-24 13:47:08 -0400

    Adding nerd tree for nvim

--- build/home/blong14/.config/nvim/lua/blong14/packer.lua
+++ build/home/blong14/.config/nvim/lua/blong14/packer.lua
@@ -43,4 +43,6 @@ return require('packer').startup(function(use)
       'nvim-lualine/lualine.nvim',
       requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
+
+  use("preservim/nerdtree")
 end)
