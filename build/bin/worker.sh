e8a9b540af1b1363b82dd04e6b78563ff7d27824
Ben Long <blong14@lemur.localdomain>
2024-12-29 14:27:44 -0500

    Updates to add new node to cluster

--- build/bin/worker.sh
+++ build/bin/worker.sh
@@ -1,2 +1,2 @@
-export INSTALL_K3S_VERSION=v1.26.8+k3s1
+export INSTALL_K3S_VERSION=v1.31.4+k3s1
 curl -sfL https://get.k3s.io | K3S_TOKEN=$(cat /etc/node-token) K3S_URL=https://compute.cluster:6443 sh -s
