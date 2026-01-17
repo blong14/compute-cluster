76d218e6423f487cddd10ffcf6b0e106a998b245
Your Name <you@example.com>
2026-01-17 10:43:04 -0500

    Adding install command to cli tool

--- src/cmd/deploy.go
+++ src/cmd/deploy.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/apenella/go-ansible/v2/pkg/execute"
 	"github.com/apenella/go-ansible/v2/pkg/playbook"
@@ -25,6 +27,12 @@ var deployCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		srvc := args[0]
 		v := viper.GetViper()
+		venvPath := v.GetString("ansible.venv_path")
+		if venvPath != "" {
+			venvBin := filepath.Join(venvPath, "bin")
+			currentPath := os.Getenv("PATH")
+			os.Setenv("PATH", venvBin+":"+currentPath)
+		}
 		opts := ansible.NewAnsibleOpts(v)
 		switch srvc {
 		case "collector", "logconsumer", "mercure", "scrutiny":
